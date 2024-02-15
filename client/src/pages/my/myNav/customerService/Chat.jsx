1fe54a4621d7555283b0c6ebba6d74a3495bb19a
bftai <bftaicompany@gmail.com>
2024-02-15 23:25:24 +0600

    tooltip

--- client/src/pages/my/myNav/customerService/Chat.jsx
+++ client/src/pages/my/myNav/customerService/Chat.jsx
@@ -5,7 +5,7 @@ const Chat = () => {
   
   return (
     <div className="fixed top-0 right-0 left-0 bottom-0 w-full px-2 mx-auto h-screen">
-      <h1 className="font-semibold text-center py-2">Chat</h1>
+      <h1 className="font-semibold text-center py-2">Customer Service</h1>
       <div className="flex flex-col justify-end chatPage">
         <ShowMessages />
         <SendBox />
--- client/src/pages/my/myNav/customerService/ShowMessages.jsx
+++ client/src/pages/my/myNav/customerService/ShowMessages.jsx
@@ -16,49 +16,63 @@ const ShowMessages = () => {
     );
   }
   return (
-      <div className="w-full overflow-y-auto no-scrollbar flex flex-col-reverse px-3">
-        {chats.map((chat) => {
-          if (chat.client && chat.client?._id == clientId) {
-            return chat.messages
-              .slice()
-              .reverse()
-              .map((message, i) => {
-                const messageStyle =
-                  message.sender == user._id
-                    ? "text-end ms-auto"
-                    : "text-start me-auto";
-                const contentStyle =
-                  message.sender == user._id
-                    ? "bg-primary text-white"
-                    : "bg-white border border-myPrimary";
-                const time = moment(message.time).format("DD/MM/YYYY HH:mm:ss");
-                return (
-                  <div key={i} className={`flex items-center gap-3 ${message.sender === user._id ? "flex-row-reverse" : "flex-row"}`}>
-                    <figure >
-                      <img src={chat.client.avatar} alt={chat.client.name} className="h-8"/>
-                    </figure>
-                    <div className={`mt-2 ${messageStyle}`}>
-                      {message.text ? (
-                        <span
-                          className={`max-w-md inline-block rounded-md px-3 py-1 ${contentStyle}`}
-                        >
-                          {message.text}
-                        </span>
-                      ) : (
-                        <img
-                          src={message.image}
-                          alt="image"
-                          className={`w-2/3 p-3 ${messageStyle}`}
-                        />
-                      )}
-                      <p className="text-xs text-gray-100">{time}</p>
-                    </div>
+    <div className="w-full overflow-y-auto no-scrollbar flex flex-col-reverse px-3">
+      {chats.map((chat) => {
+        if (chat.client && chat.client?._id == clientId) {
+          return chat.messages
+            .slice()
+            .reverse()
+            .map((message, i) => {
+              const messageStyle =
+                message.sender == user._id
+                  ? "text-end ms-auto"
+                  : "text-start me-auto";
+              const contentStyle =
+                message.sender == user._id
+                  ? "bg-primary text-white"
+                  : "bg-white border border-myPrimary";
+              const time = moment(message.time).format("DD/MM/YYYY HH:mm:ss");
+              const name = message.sender === user._id ? user.name : chat.client?.name;
+              const avatar = message.sender === user._id ? user.avatar : chat.client?.avatar;
+              return (
+                <div
+                  key={i}
+                  className={`flex items-center gap-3 tooltip ${
+                    message.sender === user._id
+                      ? "flex-row-reverse"
+                      : "flex-row"
+                  } `}
+                  data-tip={name}
+                >
+                  <figure >
+                    <img
+                      src={avatar}
+                      alt={name}
+                      className="h-8"
+                    />
+                  </figure>
+                  <div className={`mt-2 ${messageStyle}`}>
+                    {message.text ? (
+                      <span
+                        className={`max-w-md inline-block rounded-md px-3 py-1 ${contentStyle}`}
+                      >
+                        {message.text}
+                      </span>
+                    ) : (
+                      <img
+                        src={message.image}
+                        alt="image"
+                        className={`w-2/3 p-3 ${messageStyle}`}
+                      />
+                    )}
+                    <p className="text-xs text-gray-100">{time}</p>
                   </div>
-                );
-              });
-          }
-        })}
-      </div>
+                </div>
+              );
+            });
+        }
+      })}
+    </div>
   );
 };
 
