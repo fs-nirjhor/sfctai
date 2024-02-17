687881c5203d525e5240074ecb63de8fbda60318
bftai <bftaicompany@gmail.com>
2024-02-17 19:33:56 +0600

    a tag download

--- client/src/pages/my/myOption/DownloadApp.jsx
+++ client/src/pages/my/myOption/DownloadApp.jsx
@@ -5,14 +5,15 @@ import { allApi } from "../../../router/axiosApi";
 import { BsDownload } from "react-icons/bs";
 import { serverUrl } from "../../../data/config";
 
+
 const DownloadApp = () => {
 
   const handleClick = async () => {
     event.preventDefault();
     try {
-    await allApi.get("download-app");
+    //await allApi.get("download-app");
       window.location.assign(`${serverUrl}/api/download-app`);
-      toast.success("Download Started");
+      //toast.success("Download Started");
     } catch (err) {
       if (err.response?.data.message) {
         toast.error(err.response.data.message); // error sent by server
@@ -24,8 +25,7 @@ const DownloadApp = () => {
 
   return (
     <>
-    <Link 
-      to={`${serverUrl}/api/download-app`} target="_blank"
+    <a href={`${serverUrl}/api/download-app`} download
       className="flex justify-between p-3 border-b-2 border-b-mySecondary text-black font-serif"
     >
       <p className="text-start">
@@ -33,7 +33,7 @@ const DownloadApp = () => {
         <span>Download App</span>
       </p>
       <IoChevronForward />
-    </Link>
+    </a>
     </>
   );
 };
