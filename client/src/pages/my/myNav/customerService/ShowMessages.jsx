dc5dbdd189d21ed6addba63e4013a3c3c0b8c897
bftai <bftaicompany@gmail.com>
2024-02-18 20:00:54 +0600

    chat avatar and image size

--- client/src/pages/my/myNav/customerService/ShowMessages.jsx
+++ client/src/pages/my/myNav/customerService/ShowMessages.jsx
@@ -1,12 +1,19 @@
-import { useParams, useRouteLoaderData } from "react-router-dom";
+import { useNavigate, useParams, useRouteLoaderData } from "react-router-dom";
 import UseChat from "./UseChat";
 import moment from "moment";
 
 const ShowMessages = () => {
   const user = useRouteLoaderData("user");
+  const navigate = useNavigate()
   const { chats } = UseChat();
   const { client } = useParams();
   const clientId = client || user._id;
+  const handleClick = (userId) => {
+    event.preventDefault();
+    if (user.isAdmin) {
+      navigate(`/client/${userId}`);
+    }
+  };
 
   if (!chats || !chats.length) {
     return (
@@ -44,14 +51,14 @@ const ShowMessages = () => {
                   } `}
                   
                 >
-                  <figure className="tooltip" data-tip={name}>
+                  <figure onClick={() => handleClick(chat.client?.userId)} className="flex-none">
                     <img
                       src={avatar}
                       alt={name}
-                      className="w-8 h-8"
+                      className="w-10"
                     />
                   </figure>
-                  <div className={`mt-2 tooltip ${messageStyle}`} data-tip={name}>
+                  <div className={`mt-2 ${messageStyle}`}>
                     {message.text ? (
                       <span
                         className={`max-w-md inline-block rounded-md px-3 py-1 ${contentStyle}`}
@@ -62,7 +69,7 @@ const ShowMessages = () => {
                       <img
                         src={message.image}
                         alt="image"
-                        className={`w-2/3 p-3 ${messageStyle}`}
+                        className={`max-w-xs p-3 ${messageStyle}`}
                       />
                     )}
                     <p className="text-xs text-gray-100">{time}</p>
