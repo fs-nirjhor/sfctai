406c2a0f789df084fe9f2639f6b87325a4a38fa9
bftai <bftaicompany@gmail.com>
2024-02-20 09:09:25 +0600

    chat not white

--- client/src/pages/my/myNav/customerService/Chat.jsx
+++ client/src/pages/my/myNav/customerService/Chat.jsx
@@ -1,17 +1,10 @@
-import { useNavigate } from "react-router-dom";
 import SendBox from "./SendBox";
 import ShowMessages from "./ShowMessages";
-import { VscChromeClose } from "react-icons/vsc";
 
 const Chat = () => {
-  const navigate = useNavigate()
   return (
-          <div className="flex flex-col justify-between w-full h-[calc(100vh-8rem)] sm:h-[calc(100vh-7rem)] md:h-[calc(100vh-6rem)] lg:h-[calc(100vh-5rem)] p-2 max-w-md rounded-md shadow-md mx-auto bg-white">
-          <div className="pb-2 inline-flex justify-between">
-            <span></span>
-            <h1 className="font-semibold text-center h1">Customer Service</h1>
-            <span className="text-end" onClick={() => navigate(-1)}><VscChromeClose /></span>
-          </div>
+          <div className="flex flex-col justify-between w-full max-w-md mx-auto h-[calc(100vh-8rem)] sm:h-[calc(100vh-7rem)] md:h-[calc(100vh-6rem)] lg:h-[calc(100vh-5rem)] p-2">
+            <h1 className="font-semibold text-center h1 pb-2">Customer Service</h1>
             <ShowMessages />
             <SendBox />
           </div>
