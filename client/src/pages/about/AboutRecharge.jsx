914190d7465029cc2708e9b441cdbbf8d3ed834c
bftai <bftaicompany@gmail.com>
2024-03-17 10:07:14 +0600

    about withdraw

--- client/src/pages/about/AboutRecharge.jsx
+++ client/src/pages/about/AboutRecharge.jsx
@@ -8,9 +8,10 @@ const AboutRecharge = () => {
       <h1 className="font-semibold text-center pt-2 mb-5">About Recharge</h1>
       <div className="bg-mySecondary p-3 rounded-md">
         <p>
-          The minimum value of recharge is {configuration?.minimumRecharge} USDT, only USDT is
-          supported for recharge, if you use other currencies for recharge, the
-          funds will not be returned and the user will bear the loss.
+          The minimum value of recharge is{" "}
+          {configuration?.minimumRecharge || 10} USDT, only USDT is supported
+          for recharge, if you use other currencies for recharge, the funds will
+          not be returned and the user will bear the loss.
         </p>
         <Link
           to="https://www.binance.com/en/support/faq/how-to-buy-cryptocurrency-via-p2p-trading-on-binance-lite-384c0a3441b04a9cbe97c9687ef86b60"
--- client/src/pages/about/AboutWithdrawal.jsx
+++ client/src/pages/about/AboutWithdrawal.jsx
@@ -1,17 +1,17 @@
 import { useRouteLoaderData } from "react-router-dom";
 
 const AboutWithdrawal = () => {
-  const configuration =
-    useRouteLoaderData("configuration");
+  const configuration = useRouteLoaderData("configuration");
 
   return (
     <section className="px-2">
       <h1 className="font-semibold text-center pt-2 mb-5">About Withdrawal</h1>
       <p className="bg-mySecondary p-3 rounded-md">
-        The minimum withdrawal amount is {configuration?.minimumWithdraw} USDT, the withdrawal
-        fee is {configuration?.withdrawFee}% of the withdrawal amount, the platform&apos;s
-        withdrawal time is &apos;t+1&apos; mode, the number of withdrawals 1
-        time/day, the withdrawal time is {configuration?.withdrawTime} time every day.
+        The minimum withdrawal amount is {configuration?.minimumWithdraw || 6}{" "}
+        USDT, the withdrawal fee is {configuration?.withdrawFee || 3}% of the
+        withdrawal amount, the platform&apos;s withdrawal time is
+        &apos;t+1&apos; mode, the number of withdrawals 1 time/day, the
+        withdrawal time is 10:00-22:00 Saudi Arabia time every day.
       </p>
     </section>
   );
