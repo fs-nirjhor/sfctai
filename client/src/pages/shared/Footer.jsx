8e797221ef224149e8fd6bd3b4f3424430b57c4c
bftai <bftaicompany@gmail.com>
2024-04-08 08:14:45 +0600

    modify

--- client/src/pages/shared/Footer.jsx
+++ client/src/pages/shared/Footer.jsx
@@ -6,28 +6,29 @@ import { GoPerson } from "react-icons/go";
 const Footer = () => {
   const iconStyle = "h-7 w-10";
   const textStyle = "btm-nav-label first-letter:text-lg";
+  const navlinkStyle = "text-white";
   return (
     <footer>
       <nav
         className="btm-nav max-w-4xl mx-auto bg-myBg font-mono text-sm"
         id="footer-nav"
       >
-        <NavLink to="/" className="text-white">
+        <NavLink to="/" className={navlinkStyle}>
           <HiOutlineHome className={iconStyle} />
           <span className={textStyle}>HOME</span>
         </NavLink>
-        <NavLink to="trade" className="text-white">
+        <NavLink to="trade" className={navlinkStyle}>
           <TbDeviceDesktopAnalytics className={iconStyle} />
           <p className={textStyle}>
             <span>COPY </span>
             <span className="text-xl">T</span>RADE
           </p>
         </NavLink>
-        <NavLink to="about" className="text-white">
+        <NavLink to="about" className={navlinkStyle}>
           <HiOutlineClipboardDocumentList className={iconStyle} />
           <span className={textStyle}>ABOUT </span>
         </NavLink>
-        <NavLink to="my" className="text-white">
+        <NavLink to="my" className={navlinkStyle}>
           <GoPerson className={iconStyle} />
           <span className={textStyle}>MY</span>
         </NavLink>
