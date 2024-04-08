1ad9ed6941b12b59f124d75bda6b54a01075e5a6
bftai <bftaicompany@gmail.com>
2024-04-08 13:14:32 +0600

    authenticated

--- client/src/pages/my/myOption/clients/HandleAuthentication.jsx
+++ client/src/pages/my/myOption/clients/HandleAuthentication.jsx
@@ -1,14 +1,46 @@
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { userApi } from "../../../../router/axiosApi";
+
 const HandleAuthentication = ({ client, setReload }) => {
+  const [processing, setProcessing] = useState(false);
+  const isAuthenticated = client?.authentication?.isAuthenticated;
   const isPhoto =
     client?.authentication?.frontPhoto && client?.authentication?.backPhoto;
-  const isAuthenticated = client?.authentication?.isAuthenticated;
+  const handleAuthentication = async () => {
+    event.preventDefault();
+    try {
+      if (!processing) {
+        const data = { "authentication.isAuthenticated": !isAuthenticated };
+        setProcessing(true);
+        const res = await userApi.put(client?._id, data);
+        setProcessing(false);
+        if (res.data?.success) {
+          toast.success("Authentication updated");
+          setReload((prev) => prev + 1);
+        }
+      }
+    } catch (error) {
+      toast.error(error.message);
+      setProcessing(false);
+    }
+  };
   return (
     <section>
       <div className="flex justify-between">
-        <h3 className="mb-3 font-semibold">
+        <h3
+          className={`mb-3 font-semibold ${
+            isAuthenticated ? "text-green-500" : "text-red-500"
+          }`}
+        >
           {isAuthenticated ? "Authenticated" : "Not Authenticated"}
         </h3>
-        <input type="checkbox" className="toggle toggle-success" />
+        <input
+          type="checkbox"
+          className="toggle toggle-success"
+          checked={isAuthenticated || false}
+          onChange={handleAuthentication}
+        />
       </div>
       {isPhoto ? (
         <div className="flex justify-between max-w-md gap-3 mx-auto my-2 font-semibold text-center text-xs">
