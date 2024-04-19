77b11a9d5ef48f3679366c9ccf3f99c1947ea154
bftai <bftaicompany@gmail.com>
2024-04-19 21:26:35 +0600

    headline

--- client/src/pages/homepage/Headline.jsx
+++ client/src/pages/homepage/Headline.jsx
@@ -2,12 +2,16 @@ const Headline = () => {
   return (
     <section className="flex flex-row bg-white py-2 px-3 mt-10 ">
       <figure>
-        <img src="/images/mike.png" alt="mike" className="w-10 me-5"/>
+        <img src="/images/mike.png" alt="mike" className="w-10 me-5" />
       </figure>
-      <p className="uppercase font-semibold flex-grow">
-        <marquee>Welcome to <span className="text-lg">N</span>ew <span className="text-lg">C</span>opy <span className="text-lg">T</span>rading <span className="text-lg">P</span>latform</marquee>
+      <p className="font-semibold flex-grow">
+        <marquee>Welcome To New Crypto Exchanger Platform</marquee>
       </p>
     </section>
   );
 };
 export default Headline;
+
+{
+  /* <marquee>Welcome to <span className="text-lg">N</span>ew <span className="text-lg">C</span>opy <span className="text-lg">T</span>rading <span className="text-lg">P</span>latform</marquee> */
+}
