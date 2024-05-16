fc0cfc9687d9a61e4ca819f9f299638e4d8d6d76
bftai <bftaicompany@gmail.com>
2024-05-16 15:42:17 +0600

    pwa installation setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: "AFTAAI",
+        short_name: "AFTAAI",
+        description: "AFTAAI is a best trading platform",
+        theme_color: "#f9fafb",
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
   envDir: "../",
-})
+});
