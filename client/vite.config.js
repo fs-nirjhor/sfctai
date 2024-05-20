7c523faf18047997a8ae2a191747e8ea5295e7ad
bftai <bftaicompany@gmail.com>
2024-05-20 10:19:05 +0600

    injectRegister: "auto", from false

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      injectRegister: false,
+      injectRegister: "auto",
 
       pwaAssets: {
         disabled: false,
